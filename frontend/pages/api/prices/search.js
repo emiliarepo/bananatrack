da4ae90c967fd7249e4d7ff99d59a8182f5c4c6d
Emilia Repo <emiliarepo@icloud.com>
2022-02-01 00:00:11 +0200

    limit search query length

--- frontend/pages/api/prices/search.js
+++ frontend/pages/api/prices/search.js
@@ -5,6 +5,5 @@ export default async function handler(req, res) {
     const fuse = new Fuse(stores, {
         keys: [{name: 'spotName', weight: 0.5}, 'address', 'brand', {name: 'name', weight: 2}],
     })
-    console.log(req.query.q.toString())
-    res.status(200).json(fuse.search(req.query.q).slice(0, 5))
+    res.status(200).json(fuse.search(req.query.q.slice(0, 25)).slice(0, 5))
 }
