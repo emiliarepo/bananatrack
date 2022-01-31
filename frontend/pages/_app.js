4e37a3832a2eedd182baebc2cc78b087e48906d9
Emilia Repo <emiliarepo@icloud.com>
2022-01-31 16:10:48 +0200

    anal

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,46 @@
+import React from 'react'
+import ReactGA from 'react-ga'
 import '../styles/globals.css'
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout><Component {...pageProps} /></Layout>
+}
+
+class Layout extends React.Component {
+  componentDidMount() {
+    if (!window.GA_INITIALIZED) {
+      initGA()
+      window.GA_INITIALIZED = true
+      logPageView()
+    }
+  }
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+
+const initGA = () => {
+  console.log('GA init')
+  ReactGA.initialize('UA-218938223-1')
+}
+const logPageView = () => {
+  console.log(`Logging pageview for ${window.location.pathname}`)
+  ReactGA.set({ page: window.location.pathname })
+  ReactGA.pageview(window.location.pathname)
+}
+const logEvent = (category = '', action = '') => {
+  if (category && action) {
+    ReactGA.event({ category, action })
+  }
+}
+const logException = (description = '', fatal = false) => {
+  if (description) {
+    ReactGA.exception({ description, fatal })
+  }
 }
