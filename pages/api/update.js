af386049ad1aa5466b08a37a99275004bf8c88ec
Emilia Repo <emiliarepo@icloud.com>
2022-02-01 11:02:36 +0200

    remove debug

--- pages/api/update.js
+++ pages/api/update.js
@@ -3,10 +3,7 @@ import { githubSecret } from "./../../config";
 
 export default function handler(req, res) {
 	try {
-		console.log(githubSecret);
-		console.log(req.headers.authorization);
 		const actionKey = req.headers.authorization.split(" ")[1];
-		console.log(actionKey);
 		if (actionKey === githubSecret) {
 			updatePrices();
 			res.status(200).json({ success: "true" });
